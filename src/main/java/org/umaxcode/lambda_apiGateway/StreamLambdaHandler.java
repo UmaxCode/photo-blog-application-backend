62eed7ab928cf8e3eb6c9038e288641fb3b6bae9
umaxcode <maxwellodoom1729@gmail.com>
2025-01-27 14:07:54 +0000

    refactor: make handler variable final

--- src/main/java/org/umaxcode/lambda_apiGateway/StreamLambdaHandler.java
+++ src/main/java/org/umaxcode/lambda_apiGateway/StreamLambdaHandler.java
@@ -14,7 +14,7 @@ import java.io.OutputStream;
 
 
 public class StreamLambdaHandler implements RequestStreamHandler {
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private final static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
