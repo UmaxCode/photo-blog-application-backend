8ffe8351d8b6d29eb0b4c11239325071e063d82d
umaxcode <maxwellodoom1729@gmail.com>
2025-02-03 13:45:46 +0000

    feat: add notification implementation when processing of image fails

--- statemachine/src/main/java/org/umaxcode/ImageProcessingFailureNotificationFunction.java
+++ statemachine/src/main/java/org/umaxcode/ImageProcessingFailureNotificationFunction.java
@@ -3,7 +3,10 @@ package org.umaxcode;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ImageProcessingFailureNotificationFunction implements RequestHandler<Map<String, Object>, Void> {
@@ -19,28 +22,29 @@ public class ImageProcessingFailureNotificationFunction implements RequestHandle
     @Override
     public Void handleRequest(Map<String, Object> event, Context context) {
 
-//        String email = (String) event.get("email");
-//        String objectKey = (String) event.get("objectKey");
-//
-//        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
-//        messageAttributes.put("endpointEmail", MessageAttributeValue.builder()
-//                .dataType("String")
-//                .stringValue(email)
-//                .build());
-//
-//        PublishRequest publishRequest = PublishRequest.builder()
-//                .topicArn(topicArn)
-//                .subject("Image processing Failed")
-//                .message("There was an error processing the image")
-//                .messageAttributes(messageAttributes)
-//                .build();
-//
-//        // Publish the message
-//        try {
-//            snsClient.publish(publishRequest);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        String email = (String) event.get("email");
+        String objectKey = (String) event.get("objectKey");
+
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+        messageAttributes.put("endpointEmail", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue("xidide7271@eluxeer.com")
+                .build());
+
+        PublishRequest publishRequest = PublishRequest.builder()
+                .topicArn(topicArn)
+                .subject("Image processing Failed")
+                .message("There was an error processing the image: " + objectKey)
+                .messageAttributes(messageAttributes)
+                .build();
+
+        // Publish the message
+        try {
+            snsClient.publish(publishRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         context.getLogger().log("Event" + event);
         context.getLogger().log("Send processing failed notification");
         return null;
