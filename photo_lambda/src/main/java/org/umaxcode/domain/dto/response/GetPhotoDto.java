0cab4ff8ada4d50584368d9659e7a62b3ea248d7
umaxcode <maxwellodoom1729@gmail.com>
2025-02-07 12:28:38 +0000

    refactor: change GetPhotoDto from record to class

--- photo_lambda/src/main/java/org/umaxcode/domain/dto/response/GetPhotoDto.java
+++ photo_lambda/src/main/java/org/umaxcode/domain/dto/response/GetPhotoDto.java
@@ -1,10 +1,13 @@
 package org.umaxcode.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
-public record GetPhotoDto(
-        String imgId,
-        byte[] image
-) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class GetPhotoDto {
+    private String imgId;
+    private byte[] image;
 }
