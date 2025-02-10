415e48e1df7fb297c99bbf1e2fb5ac56a0933f8b
umaxcode <maxwellodoom1729@gmail.com>
2025-02-10 07:23:07 +0000

    feat: extract email, firstName and lastName for jwt token during image upload

--- photo_lambda/src/main/java/org/umaxcode/service/PhotoBlogService.java
+++ photo_lambda/src/main/java/org/umaxcode/service/PhotoBlogService.java
@@ -1,5 +1,6 @@
 package org.umaxcode.service;
 
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.multipart.MultipartFile;
 import org.umaxcode.domain.dto.response.GetPhotoDto;
 import org.umaxcode.domain.dto.response.PhotoUploadDTo;
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface PhotoBlogService {
 
-    String upload(MultipartFile pic);
+    String upload(MultipartFile pic, Jwt jwt);
 
     PhotoUploadDTo generatePreSignedUrl(String id);
 
