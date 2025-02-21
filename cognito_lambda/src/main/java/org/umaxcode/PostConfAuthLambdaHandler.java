bcc22522d08f7985947d671423908daef85126e5
umaxcode <maxwellodoom1729@gmail.com>
2025-02-21 12:37:11 +0000

    feat: add lambda function to decouple post confirmation and authentication processing using sqs

--- cognito_lambda/src/main/java/org/umaxcode/PostConfAuthLambdaHandler.java
+++ cognito_lambda/src/main/java/org/umaxcode/PostConfAuthLambdaHandler.java
@@ -6,37 +6,29 @@ import com.amazonaws.services.lambda.runtime.events.CognitoUserPoolPostAuthentic
 import com.amazonaws.services.lambda.runtime.events.CognitoUserPoolPostConfirmationEvent;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
-import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sns.model.*;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
 public class PostConfAuthLambdaHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
     private final ObjectMapper objectMapper;
-    private final SnsClient snsClient;
     private final String topicArn;
     private final String secondaryUserPoolId;
-    private final CognitoIdentityProviderClient cognitoIdentityProviderClient;
+    private final SqsClient sqsClient;
+    private final String queueUrl;
 
     public PostConfAuthLambdaHandler() {
         this.objectMapper = new ObjectMapper();
         this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        this.snsClient = SnsClient.create();
+        this.sqsClient = SqsClient.create();
         this.topicArn = System.getenv("SNS_NOTIFICATION_TOPIC_ARN");
         this.secondaryUserPoolId = System.getenv("SECONDARY_USER_POOL_ID");
-        this.cognitoIdentityProviderClient = CognitoIdentityProviderClient.builder()
-                .region(Region.of(System.getenv("SECONDARY_REGION")))
-                .build();
+        this.queueUrl = System.getenv("QUEUE_URL");
     }
 
     @Override
@@ -44,136 +36,92 @@ public class PostConfAuthLambdaHandler implements RequestHandler<Map<String, Obj
         context.getLogger().log("Event" + event);
         String triggerSource = (String) event.get("triggerSource");
 
+        // Add attributes to the message
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+
         switch (triggerSource) {
             case "PostConfirmation_ConfirmSignUp":
                 CognitoUserPoolPostConfirmationEvent postConfirmationEvent = objectMapper.convertValue(event, CognitoUserPoolPostConfirmationEvent.class);
-                handlePostConfirmation(postConfirmationEvent, context);
+                handlePostConfirmation(postConfirmationEvent, messageAttributes, context);
                 break;
             case "PostAuthentication_Authentication":
                 CognitoUserPoolPostAuthenticationEvent postAuthenticationEvent = objectMapper.convertValue(event, CognitoUserPoolPostAuthenticationEvent.class);
-                handlePostAuthentication(postAuthenticationEvent, context);
+                handlePostAuthentication(postAuthenticationEvent, messageAttributes, context);
                 break;
             default:
                 context.getLogger().log("Unhandled Cognito event trigger: " + triggerSource);
-                break;
+                return event;
         }
+
+        messageAttributes.put("triggerSource", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(triggerSource)
+                .build());
+
+        messageAttributes.put("topicArn", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(topicArn)
+                .build());
+
+        // Send the message to SQS with attributes
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody("Cognito message for sqs")
+                .messageAttributes(messageAttributes)
+                .build();
+
+        sqsClient.sendMessage(sendMessageRequest);
 
         return event;
     }
 
-    private void handlePostConfirmation(CognitoUserPoolPostConfirmationEvent event, Context context) {
+    private void handlePostAuthentication(CognitoUserPoolPostAuthenticationEvent event, Map<String, MessageAttributeValue> sqsMessage, Context context) {
         String email = event.getRequest().getUserAttributes().get("email");
-        sendSNSNotification(email, context);
-        replicateUserDetailInSecondaryRegion(event, context);
-    }
-
-    private void sendSNSNotification(String email, Context context) {
-
-        SubscribeRequest subscribeRequest = SubscribeRequest.builder()
-                .protocol("email")
-                .endpoint(email)
-                .returnSubscriptionArn(true)
-                .topicArn(topicArn)
-                .build();
-
-        try {
-            SubscribeResponse response = snsClient.subscribe(subscribeRequest);
-            context.getLogger().log("Subscription result: " + response);
-
-            // Define a filter policy
-            String filterPolicy = String.format("{ \"endpointEmail\": [\"%s\"] }", email);
-
-            // Set the filter policy for the subscription
-            String subscriptionArn = response.subscriptionArn();
-
-            System.out.println("SubscriptionArn" + subscriptionArn);
-            SetSubscriptionAttributesRequest filterPolicyRequest = SetSubscriptionAttributesRequest.builder()
-                    .subscriptionArn(subscriptionArn)
-                    .attributeName("FilterPolicy")
-                    .attributeValue(filterPolicy)
-                    .build();
-
-            snsClient.setSubscriptionAttributes(filterPolicyRequest);
-
-            context.getLogger().log("Filter policy set for subscription: " + subscriptionArn);
-            context.getLogger().log("Successfully subscribed " + email + " to the SNS topic with filter policy: " + topicArn);
-
-        } catch (Exception e) {
-            context.getLogger().log("Error subscribing user: " + e.getMessage());
-        }
-
-    }
-
-    private void replicateUserDetailInSecondaryRegion(CognitoUserPoolPostConfirmationEvent event, Context context) {
-
-        String userPoolId = event.getUserPoolId();
-        String username = event.getUserName();
-        Map<String, String> userAttributesFromEvent = event.getRequest().getUserAttributes();
-        if (secondaryUserPoolId.isEmpty()) { // avoid replicating user pool details from
-            // secondary region to primary region
-            return;
-        }
-
-        try {
-
-            List<AttributeType> userAttributes = new ArrayList<>();
-            userAttributes.add(AttributeType.builder().name("email").value(userAttributesFromEvent.get("email")).build());
-            userAttributes.add(AttributeType.builder().name("family_name").value(userAttributesFromEvent.get("family_name")).build());
-            userAttributes.add(AttributeType.builder().name("given_name").value(userAttributesFromEvent.get("given_name")).build());
-            userAttributes.add(AttributeType.builder().name("email_verified").value("true").build());
-
-            AdminCreateUserRequest createUserRequest = AdminCreateUserRequest.builder()
-                    .userPoolId(userPoolId)
-                    .username(username)
-                    .userAttributes(userAttributes)
-                    .build();
-
-            cognitoIdentityProviderClient.adminCreateUser(createUserRequest);
-        } catch (Exception ex) {
-            context.getLogger().log("Error replicating user detail: " + ex.getMessage());
-        }
-    }
-
-    private void handlePostAuthentication(CognitoUserPoolPostAuthenticationEvent event, Context context) {
-        String email = event.getRequest().getUserAttributes().get("email");
-        String firstName = event.getRequest().getUserAttributes().get("given_name");
-
-        String subject = "Successful Login Notification";
-
-        String htmlMessage = String.format("""
-                   <html>
-                             <body>
-                                 <h2>Login Notification</h2>
-                                 <p>Hello <strong>%s</strong>,</p>
-                                 <p>We noticed a login to your account on <b>%s</b>.</p>
-                                 <p><strong>Login Details:</strong></p>
-                                 <ul>
-                                     <li><b>Date & Time:</b> %s</li
-                                 </ul>
-                             </body>
-                         </html>
-                """, firstName, "UmaxPhotoShare", LocalDateTime.now());
-
-        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
-        messageAttributes.put("endpointEmail", MessageAttributeValue.builder()
+        String firstname = event.getRequest().getUserAttributes().get("given_name");
+        sqsMessage.put("email", MessageAttributeValue.builder()
                 .dataType("String")
                 .stringValue(email)
                 .build());
 
-        PublishRequest publishRequest = PublishRequest.builder()
-                .subject(subject)
-                .topicArn(topicArn)
-                .message(htmlMessage)
-                .messageAttributes(messageAttributes)
-                .build();
+        sqsMessage.put("firstname", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(firstname)
+                .build());
 
-        try {
-            snsClient.publish(publishRequest);
-        } catch (Exception ex) {
-            context.getLogger().log("Error publishing sns to user: " + ex.getMessage());
-        }
+        context.getLogger().log("Preparing post authentication sqs message");
+    }
 
-        String userId = event.getUserName();
-        context.getLogger().log("User {} successfully authenticated: " + userId);
+    private void handlePostConfirmation(CognitoUserPoolPostConfirmationEvent event, Map<String, MessageAttributeValue> sqsMessage, Context context) {
+        String email = event.getRequest().getUserAttributes().get("email");
+        String username = event.getUserName();
+        String given_name = event.getRequest().getUserAttributes().get("given_name");
+        String family_name = event.getRequest().getUserAttributes().get("family_name");
+
+        sqsMessage.put("email", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(email)
+                .build());
+
+        sqsMessage.put("username", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(username)
+                .build());
+
+        sqsMessage.put("firstname", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(given_name)
+                .build());
+
+        sqsMessage.put("lastname", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(family_name)
+                .build());
+
+        sqsMessage.put("secondaryCognitoPoolId", MessageAttributeValue.builder()
+                .dataType("String")
+                .stringValue(this.secondaryUserPoolId)
+                .build());
+
+        context.getLogger().log("Preparing post confirmation sqs message");
     }
 }
