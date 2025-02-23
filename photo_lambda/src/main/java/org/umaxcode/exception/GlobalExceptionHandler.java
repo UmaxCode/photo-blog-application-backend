d9d740b1552fc270d4672e252412bcca16699cc7
umaxcode <maxwellodoom1729@gmail.com>
2025-02-23 11:39:14 +0000

    feat: add global exception handling for max file upload size

--- photo_lambda/src/main/java/org/umaxcode/exception/GlobalExceptionHandler.java
+++ photo_lambda/src/main/java/org/umaxcode/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package org.umaxcode.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.time.LocalDateTime;
 
@@ -18,6 +20,17 @@ public class GlobalExceptionHandler {
         return ErrorResponse.builder()
                 .path(request.getRequestURI())
                 .message(ex.getMessage())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+    public ErrorResponse maxPictureSizeExceptionHandle(MaxUploadSizeExceededException ex, HttpServletRequest request) {
+
+        return ErrorResponse.builder()
+                .path(request.getRequestURI())
+                .message("Picture size exceeds the allowed limit! - 5M")
                 .timestamp(LocalDateTime.now().toString())
                 .build();
     }
